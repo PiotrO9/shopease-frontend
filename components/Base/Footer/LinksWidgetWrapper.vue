58c0ce82937a7b8ef95125b8836adb7b34d90e4b
PiotrO9 <piotr.ostrowski02@wp.pl>
2025-02-11 21:40:50 +0100

    refactor: split footer into smaller components

---
+++ components/Base/Footer/LinksWidgetWrapper.vue
@@ -0,0 +1,115 @@
+<template>
+	<div class="grid grid-cols-2 lg:grid-cols-4 gap-8">
+		<div class="flex flex-col gap-4">
+			<h3 class="text-gray500 font-bold">
+				Shopease
+			</h3>
+			<ul class="flex flex-col gap-2">
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						About
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Career
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Blog
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						B2B
+					</NuxtLink>
+				</li>
+			</ul>
+		</div>
+
+		<div class="flex flex-col gap-4">
+			<h3 class="text-gray500 font-bold">
+				Shopease
+			</h3>
+			<ul class="flex flex-col gap-2">
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						About
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Career
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Blog
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						B2B
+					</NuxtLink>
+				</li>
+			</ul>
+		</div>
+
+		<div class="flex flex-col gap-4">
+			<h3 class="text-gray500 font-bold">
+				Shopease
+			</h3>
+			<ul class="flex flex-col gap-2">
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						About
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Career
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Blog
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						B2B
+					</NuxtLink>
+				</li>
+			</ul>
+		</div>
+
+		<div class="flex flex-col gap-4">
+			<h3 class="text-gray500 font-bold">
+				Shopease
+			</h3>
+			<ul class="flex flex-col gap-2">
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						About
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Career
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						Blog
+					</NuxtLink>
+				</li>
+				<li class="inline-block">
+					<NuxtLink to="/" class="text-white">
+						B2B
+					</NuxtLink>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
---
+++ components/Base/Footer/SocialMediaWidgetWrapper.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex gap-4">
+		<NuxtLink to="/">
+			<Icon name="ic:baseline-facebook" class="size-6 bg-gray500" />
+		</NuxtLink>
+		<NuxtLink to="/">
+			<Icon name="mdi:youtube" class="size-6 bg-gray500" />
+		</NuxtLink>
+		<NuxtLink to="/">
+			<Icon name="mdi:twitter" class="size-6 bg-gray500" />
+		</NuxtLink>
+		<NuxtLink to="/">
+			<Icon name="mdi:instagram" class="size-6 bg-gray500" />
+		</NuxtLink>
+	</div>
+</template>
