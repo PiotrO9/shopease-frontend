35e2cbf4acf59e4392d6523216489730a27b545c
PiotrO9 <piotr.ostrowski02@wp.pl>
2025-02-11 21:30:15 +0100

    feat: add footer component

---
+++ components/Base/Footer/AppFooter.vue
@@ -0,0 +1,133 @@
+<template>
+	<footer class="w-full bg-primary py-8">
+		<div class="grid grid-cols-1 lg:grid-cols-[30%_70%] container gap-8 px-4">
+			<div class="flex flex-col gap-8 lg:gap-0 justify-between">
+				<HeaderLogo text-color="text-white" />
+				<div class="flex gap-4">
+					<NuxtLink to="/">
+						<Icon name="ic:baseline-facebook" class="size-6 bg-gray500" />
+					</NuxtLink>
+					<NuxtLink to="/">
+						<Icon name="mdi:youtube" class="size-6 bg-gray500" />
+					</NuxtLink>
+					<NuxtLink to="/">
+						<Icon name="mdi:twitter" class="size-6 bg-gray500" />
+					</NuxtLink>
+					<NuxtLink to="/">
+						<Icon name="mdi:instagram" class="size-6 bg-gray500" />
+					</NuxtLink>
+				</div>
+			</div>
+			<div class="grid grid-cols-2 lg:grid-cols-4 gap-8">
+				<div class="flex flex-col gap-4">
+					<h3 class="text-gray500 font-bold">
+						Shopease
+					</h3>
+					<ul class="flex flex-col gap-2">
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								About
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Career
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Blog
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								B2B
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
+				<div class="flex flex-col gap-4">
+					<h3 class="text-gray500 font-bold">
+						Shopease
+					</h3>
+					<ul class="flex flex-col gap-2">
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								About
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Career
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Blog
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								B2B
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
+				<div class="flex flex-col gap-4">
+					<h3 class="text-gray500 font-bold">
+						Shopease
+					</h3>
+					<ul class="flex flex-col gap-2">
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								About
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Career
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Blog
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								B2B
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
+				<div class="flex flex-col gap-4">
+					<h3 class="text-gray500 font-bold">
+						Shopease
+					</h3>
+					<ul class="flex flex-col gap-2">
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								About
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Career
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								Blog
+							</NuxtLink>
+						</li>
+						<li class="inline-block">
+							<NuxtLink to="/" class="text-white">
+								B2B
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+</template>
--- components/Base/Header/HeaderLogo.vue
+++ components/Base/Header/HeaderLogo.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+interface Props {
+    // Tailwind class for text color
+    textColor?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    textColor: 'text-black',
+});
+</script>
+
 <template>
 	<div class="inline-flex h-max w-max">
-		<span class="text-4xl font-bold select-none">Shopify.com</span>
+		<span class="text-4xl font-bold select-none" :class="props.textColor">Shopify.com</span>
 	</div>
 </template>
