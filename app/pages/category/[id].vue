3bef4ac6b14c94535da9fc30945a641d0bf6fbfd
PiotrO9 <piotr.ostrowski02@wp.pl>
2025-04-22 19:19:03 +0200

    fix: add empty pages for remove warning about missing route in console

---
+++ app/pages/category/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { id } = useRoute().params;
+
+definePageMeta({
+	colorMode: 'light',
+});
+</script>
+
+<template>
+	<div>
+		<h1>Category {{ id }}</h1>
+	</div>
+</template>
---
+++ app/pages/category/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+	colorMode: 'light',
+});
+</script>
+
+<template>
+	<div>
+		<h1>Category</h1>
+	</div>
+</template>
